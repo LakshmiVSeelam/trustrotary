18eee65aec2c29f8b59c3dae0987e611f878f919
LakshmiVSeelam <lakshmivseelam@gmail.com>
2022-01-12 20:41:32 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit{
   constructor(private router : Router) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd ) {
+        console.log(event.url)
         if(event.url == '/home'){
           document.body.style.background = "url('assets/img/body_bg.jpg') repeat ";
           document.body.style.backgroundSize = "contain";
