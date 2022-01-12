95e4738690beaeeaf137bd6d7b539551433d312e
LakshmiVSeelam <lakshmivseelam@gmail.com>
2022-01-12 22:40:08 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,9 @@ declare const banner_carouselFn : any;
 export class HomeComponent implements OnInit {
 
   carousel_data : any = [
+    {'img': 'buildingcarousel', 'link': '/about', 'heading': '','content': ''},
     {'img': 'bloodbankcarousel', 'link': '/programs/bloodbank', 'heading': 'Donate blood. Save life.','content': 'There is a hope of life to someone in your blood donation'},
-    {'img': 'physiocarousel' , 'link': '/programs/physiotherapy', 'heading': 'You are not sick. You are injured.', 'content': 'Activity and therapy help healing most'},
+    {'img': 'physiocarousel' , 'link': '/programs/physiotherapy', 'heading': 'You are not sick. You are injured.', 'content': 'Inhale Courage. Exhale Fear'},
     {'img': 'skillcarousel', 'link': '/programs/skilldevelopment', 'heading': 'Invest in yourself. It pays the best interest.', 'content': 'Fortunate are the few who can master a skill'}
   ]
   
