aa76e58644c4b0da5b257324fadea224038a1095
LakshmiVSeelam <lakshmivseelam@gmail.com>
2022-01-18 12:46:16 +0530

    changes

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -10,7 +10,12 @@ export class GalleryComponent implements OnInit {
   data : any;
   constructor() { 
     this.data = [
-      {'name': '', 'imgUrl': ''}
+      {'name': 'Cement Benches', 'imgUrl': 'bench_1'},
+      {'name': 'Cement Benches', 'imgUrl': 'bench_2'},
+      {'name': 'Buttermilk Distribution', 'imgUrl': 'buttermilk_1'},
+      {'name': 'Buttermilk Distribution', 'imgUrl': 'buttermilk_2'},
+      {'name': 'Blood Donation', 'imgUrl': 'blood_1'},
+      {'name': 'Blood Donation', 'imgUrl': 'blood_2'}      
     ]
   }
 
